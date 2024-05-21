e54856fc0d9a885c036082dec5d10b4ce4258a52
Himanshu Prajapati <himanshuprajapati8417@gmail.com>
2024-05-21 09:47:51 +0530

    appwrite setup completed

--- writeMe/src/App.jsx
+++ writeMe/src/App.jsx
@@ -1,29 +1,35 @@
 import { useEffect, useState } from "react";
-import conf from "./conf/conf";
-import {useDispatch} from 'react-redux'
+import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth";
-import {login, logout} from './store/authSlice'
-
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components/index.js";
 
 function App() {
-  const [loading, setLoading] = useState(true)
-  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    authService.getCurrUser()
-    .then((userData) => {
-      if (userData) {
-        dispatch(login({userData}))
-      } else {
-        dispatch(logout())
-      }
-    })
-    .finally(()=> setLoading(false))
-  },[])
-  
-  return !loading ? (<div>
+    authService
+      .getCurrUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-  </div>) : null
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>{/* outlet */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
