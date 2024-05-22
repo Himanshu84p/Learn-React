7b4f39391fc5222a86e82b79e2325233a7368241
Himanshu Prajapati <himanshuprajapati8417@gmail.com>
2024-05-22 11:19:39 +0530

    PostForm Upload

--- writeMe/src/components/index.js
+++ writeMe/src/components/index.js
@@ -2,8 +2,20 @@ import Header from "../components/Header/Header.jsx";
 import Footer from "../components/Footer/Footer.jsx";
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
-import Input from './Input.jsx'
-import Button from './Button.jsx'
-import Container from '../components/container/Container.jsx'
+import Input from "./Input.jsx";
+import Button from "./Button.jsx";
+import Container from "../components/container/Container.jsx";
+import Select from "./Select.jsx";
+import RTE from "./RTE.jsx";
 
-export { Header, Footer, Logo, LogoutBtn, Input, Button, Container };
+export {
+  Header,
+  Footer,
+  Logo,
+  LogoutBtn,
+  Input,
+  Button,
+  Container,
+  Select,
+  RTE,
+};
